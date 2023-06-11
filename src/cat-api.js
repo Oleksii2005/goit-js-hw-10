87c2ff51154ec978a33070a6a0c12d6735db7a0c
Oleksii2005 <122476976+Oleksii2005@users.noreply.github.com>
2023-06-11 09:43:29 +0200

    final commit

--- src/cat-api.js
+++ src/cat-api.js
@@ -1,2 +1,41 @@
-import { fetchBreeds } from './index.js';
-fetchBreeds();
+// export function fetchBreeds(el) {
+//   const searchQuery = el.currentTarget.elements.query.value;
+//   const url = `https://api.thecatapi.com/v1/images/search?breed_ids=${searchQuery}`;
+//   const apiKey =
+//     'live_uOaBgbBDRnjOQaHRhfDtAWgDCyLmbNQWgMDIXGo0dsPCesztzLKMTFMNPUyAVShl';
+//   return fetch(url, {
+//     headers: {
+//       'x-api-key': apiKey,
+//     },
+//   })
+//     .then(res => {
+//       if (!res.ok) {
+//         throw new Error('Error fetching breeds');
+//       } else {
+//       }
+//       return res.json();
+//     })
+//     .then(data => {
+//       return data.map(breed => ({
+//         id: breed.id,
+//         name: breed.name,
+//       }));
+//     });
+// }
+// live_uOaBgbBDRnjOQaHRhfDtAWgDCyLmbNQWgMDIXGo0dsPCesztzLKMTFMNPUyAVShl
+
+// cat-api.js
+
+const API_KEY =
+  'live_uOaBgbBDRnjOQaHRhfDtAWgDCyLmbNQWgMDIXGo0dsPCesztzLKMTFMNPUyAVShl';
+
+function fetchBreeds() {
+  const url = `https://api.thecatapi.com/v1/breeds?api_key=${API_KEY}`;
+  return fetch(url).then(res => res.json());
+}
+
+function fetchCatByBreed(breedId) {
+  const url = `https://api.thecatapi.com/v1/images/search?api_key=${API_KEY}&breed_ids=${breedId}`;
+  return fetch(url).then(res => res.json());
+}
+export { fetchBreeds, fetchCatByBreed };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,113 @@
-function fetchBreeds() {
-  return fetch(
-    'https://api.thecatapi.com/v1/breeds?apiKey=live_uOaBgbBDRnjOQaHRhfDtAWgDCyLmbNQWgMDIXGo0dsPCesztzLKMTFMNPUyAVShl'
-  ).then(res => console.log(res));
+import { fetchBreeds, fetchCatByBreed } from './cat-api.js';
+import Notiflix from 'notiflix';
+import 'notiflix/src/notiflix.css';
+import SlimSelect from 'slim-select';
+import 'slim-select/dist/slimselect.css';
+
+Notiflix.Notify.init({
+  position: 'center-top',
+  distance: '40px',
+  timeout: 3600000,
+});
+
+let eventError = false;
+
+const refs = {
+  select: document.querySelector('.breed-select'),
+  divData: document.querySelector('.cat-info'),
+};
+
+startLoading(refs.select);
+fetchBreeds()
+  .then(data => {
+    if (!data.length) throw new Error('Data not found');
+    return data.reduce(
+      (markup, currentEl) => markup + createSelectElement(currentEl),
+      ''
+    );
+  })
+  .then(updateSelect)
+  .catch(onError)
+  .finally(endLoading);
+
+refs.select.addEventListener('change', onSelect);
+
+// functions
+function createSelectElement({ id, name }) {
+  return `<option value="${id}">${name || 'Unknown'}</option>`;
 }
-export { fetchBreeds };
+
+function updateSelect(markup) {
+  refs.select.innerHTML = markup;
+  new SlimSelect({
+    select: refs.select,
+  });
+  refs.select.classList.remove('invisible');
+}
+
+function onSelect(e) {
+  startLoading(refs.divData);
+  fetchCatByBreed(e.target.value)
+    .then(data => {
+      if (!data.length) throw new Error('Data not found');
+      return data.reduce(
+        (markup, currentEl) => markup + createInfoElement(getArgs(currentEl)),
+        ''
+      );
+    })
+    .then(updateInfo)
+    .catch(onError)
+    .finally(endLoading);
+}
+
+function createInfoElement({ url, name, description, temperament }) {
+  return ` <img
+      class="cat_image"
+      src="${url}"
+      alt="${name || 'Unknown'}"
+    />
+    <div class="cat-info-text">
+    <h2>${name || 'Unknown'}</h2>
+    <p>${description || 'Unknown'}</p>
+    <p><b>Temperament: </b>${temperament || 'Unknown'}</p>
+    </div>`;
+}
+
+function updateInfo(markup) {
+  refs.divData.innerHTML = markup;
+  refs.divData.classList.remove('invisible');
+}
+
+function getArgs({ url, breeds }) {
+  const { name, description, temperament } = breeds[0];
+  return {
+    url,
+    name,
+    description,
+    temperament,
+  };
+}
+
+function startLoading(element) {
+  if (eventError) afterError();
+  element.classList.add('invisible');
+  Notiflix.Loading.hourglass('Loading data, please wait...', {
+    backgroundColor: 'rgba(0,0,0,0.6)',
+  });
+}
+
+function endLoading() {
+  Notiflix.Loading.remove();
+}
+
+function onError(error) {
+  eventError = true;
+  Notiflix.Notify.failure(error.message);
+}
+
+function afterError() {
+  const notify = document.querySelector('.notiflix-notify-failure');
+  if (notify) notify.remove();
+  eventError = false;
+}
+ч;
